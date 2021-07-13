89a6b8fa7c01b1c45632205cfa8d1b2036c02a5d
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2021-07-13 20:36:17 +0800

    Add babel

--- dist/firestore-eloquent.js
+++
@@ -1,6 +0,0 @@
-export { Model } from './model';
-export { HasRelationship } from './has-relationship';
-export { HasEvent } from './has-event';
-export * from './contracts';
-export { Collection } from './collection';
-export * from './db';
