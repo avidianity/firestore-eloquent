d4ddf0e52393971ee9b5e2648e6cb2b902846cb4
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2021-08-11 21:29:46 +0800

    Update

--- src/has-relationship.ts
+++ src/has-relationship.ts
@@ -5,21 +5,16 @@ import { BelongsTo } from './relations/belongs-to';
 import { HasMany } from './relations/has-many';
 import { HasOne } from './relations/has-one';
 
-export abstract class HasRelationship<
-	D extends ModelData
-> extends QueryBuilder<D> {
-	protected hasMany<T extends Model>(
-		relation: T,
-		name: string
-	): HasMany<T, D> {
-		return new HasMany<T, D>(relation, (this as unknown) as Model, name);
+export abstract class HasRelationship<D extends ModelData> extends QueryBuilder<D> {
+	protected hasMany<T extends Model>(relation: T, name?: string): HasMany<T, D> {
+		return new HasMany<T, D>(relation, this as unknown as Model, name);
 	}
 
-	protected hasOne<T extends Model>(relation: T, name: string): HasOne<T, D> {
-		return new HasOne<T, D>(relation, (this as unknown) as Model, name);
+	protected hasOne<T extends Model>(relation: T, name?: string): HasOne<T, D> {
+		return new HasOne<T, D>(relation, this as unknown as Model, name);
 	}
 
-	protected belongsTo<T extends Model>(parent: T, name: string) {
-		return new BelongsTo<T, D>((this as unknown) as T, parent, name);
+	protected belongsTo<T extends Model>(parent: T, name?: string) {
+		return new BelongsTo<T, D>(this as unknown as T, parent, name);
 	}
 }
